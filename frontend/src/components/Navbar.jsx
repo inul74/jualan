1665acd60d3376442b82d2dcad1e7254c58e8630
inul74 <zmusthofa74@gmail.com>
2024-09-21 23:55:07 +0700

    cart page function

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from "lucide-react";
 
 import { useUserStore } from "../stores/useUserStore";
+import { useCartStore } from "../stores/useCartStore";
 
 const Navbar = () => {
   const { user, logout } = useUserStore();
   const isAdmin = user?.role === "admin";
+  const { cart } = useCartStore();
 
   return (
     <header className="fixed top-0 left-0 w-full bg-violet-950 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
@@ -25,15 +27,20 @@ const Navbar = () => {
               Home
             </Link>
             {user && (
-              <Link className="relative group text-gray-300 hover:text-emerald-400 transition duration-300 ease-in-out">
+              <Link
+                to={"/cart"}
+                className="relative group text-gray-300 hover:text-emerald-400 transition duration-300 ease-in-out"
+              >
                 <ShoppingCart
                   className="inline-block mr-1 group-hover:text-emerald-400"
                   size={20}
                 />
                 <span className="hidden sm:inline">Cart</span>
-                <span className="absolute -top-1 left-1.5 bg-emerald-500 text-white rounded-full px-1.5 py-0.5 text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out">
-                  0
-                </span>
+                {cart.length > 0 && (
+                  <span className="absolute -top-1 left-1.5 bg-emerald-500 text-white rounded-full px-1.5 py-0.5 text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out">
+                    {cart.length}
+                  </span>
+                )}
               </Link>
             )}
             {isAdmin && (
--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -2,17 +2,21 @@ import toast from "react-hot-toast";
 import { ShoppingCart } from "lucide-react";
 
 import { useUserStore } from "../stores/useUserStore";
+import { useCartStore } from "../stores/useCartStore";
 
 const ProductCard = ({ product }) => {
   const { user } = useUserStore();
+  const { addToCart } = useCartStore();
 
   const handleAddToCart = () => {
     if (!user) {
-			toast.error("Please login to add products to your cart.", { id: "login" });
-			return;
-		} else {
-			// add to cart
-		}
+      toast.error("Please login to add products to your cart.", {
+        id: "login",
+      });
+      return;
+    } else {
+      addToCart(product);
+    }
   };
 
   return (
